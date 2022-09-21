bf744182256c2e31317aff092e6a871c485b9ff0
Andrew Tischenko <31860133+antis0007@users.noreply.github.com>
2022-09-21 15:06:27 -0600

    Add files via upload

---
+++ ext4_disable.bat
@@ -0,0 +1,8 @@
+@echo off
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+bcdedit /set testsigning off
+echo Disabled testsigning
+pause
